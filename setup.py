8157629a4309c006e0fe7a4df9735f76ed4af61f
Michael Howitz <mh@gocept.com>
2017-09-15 15:41:21 +0200

    zest.releaser compat.

--- setup.py
+++ setup.py
@@ -20,11 +20,11 @@ with open('README.rst') as f:
 with open('CHANGES.rst') as f:
     CHANGES = f.read()
 
-__version__ = '3.0a5.dev0'
+version = '3.0a5.dev0'
 
 
 setup(name='DocumentTemplate',
-      version=__version__,
+      version=version,
       url='http://pypi.python.org/pypi/DocumentTemplate',
       license='ZPL 2.1',
       description="Document Templating Markup Language (DTML)",
