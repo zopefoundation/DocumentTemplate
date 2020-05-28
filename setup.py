567a7901afdf708c790781ae652d6c5869eda7cb
Jens Vagelpohl <jens@netz.ooo>
2020-05-28 16:15:47 +0200

    - add missing python_requires

--- setup.py
+++ setup.py
@@ -61,6 +61,7 @@ setup(name='DocumentTemplate',
           "Topic :: Text Processing :: Markup",
       ],
       keywords='DTML template zope HTML SQL web markup',
+      python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
       install_requires=[
           'AccessControl >= 4.0a5',
           'Acquisition',
