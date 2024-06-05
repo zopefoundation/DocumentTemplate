8fbee5ad2662e71b03706462abcb2a663145d379
Michael Howitz <icemac@gmx.net>
2024-06-05 09:13:21 +0200

    Bumped version for breaking release.

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-4.7 (unreleased)
+5.0 (unreleased)
 ----------------
 
 - Nothing changed yet.
--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ with open('README.rst') as f:
 with open('CHANGES.rst') as f:
     CHANGES = f.read()
 
-version = '4.7.dev0'
+version = '5.0.dev0'
 
 
 setup(name='DocumentTemplate',
