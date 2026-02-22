7492faf2484f03d9e44c6ab16e506e344e8ba34f
Mauro Amico <mauro.amico@gmail.com>
2026-02-23 00:29:06 +0100

    missing builtins ?

--- src/DocumentTemplate/DT_Util.py
+++ src/DocumentTemplate/DT_Util.py
@@ -205,7 +205,7 @@ class Eval(RestrictionCapableEval):
         for name in self.used:
             __traceback_info__ = name
             # Don't try to override builtins, since they need to be protected
-            if d["__builtins__"] and name in d["__builtins__"]:
+            if d.get("__builtins__") and name in d["__builtins__"]:
                 continue
             try:
                 if name not in d:
